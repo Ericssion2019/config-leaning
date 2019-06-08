6feec86b89aa742ab19ac76d01760796fb9ef69c
Ericssion2019 <erricssion@arbfintech.com>
2019-06-08 04:14:42 -0400

    modify

--- config-server-demo/eureka-server/src/main/java/com/arbfintech/ericssion/EurekaServerApplication.java
+++ config-server-demo/eureka-server/src/main/java/com/arbfintech/ericssion/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class CongfigServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CongfigServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
