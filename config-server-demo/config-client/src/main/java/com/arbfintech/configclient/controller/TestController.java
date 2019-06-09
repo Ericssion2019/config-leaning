cb99e0264b0003b06d01c56e28bfabfc7f9e1ca2
XiaoJiaZhou2018 <git757890912>
2019-06-09 19:45:40 +0800

    config-server

---
+++ config-server-demo/config-client/src/main/java/com/arbfintech/configclient/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.arbfintech.configclient.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    private String from;
+}
