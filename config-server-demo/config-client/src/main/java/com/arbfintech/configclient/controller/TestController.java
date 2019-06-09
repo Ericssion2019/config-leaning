908946e836f47ec61994f3909176a553af785f5c
XiaoJiaZhou2018 <git757890912>
2019-06-09 20:08:17 +0800

    TestController

--- config-server-demo/config-client/src/main/java/com/arbfintech/configclient/controller/TestController.java
+++ config-server-demo/config-client/src/main/java/com/arbfintech/configclient/controller/TestController.java
@@ -1,8 +1,21 @@
 package com.arbfintech.configclient.controller;
 
+import org.omg.CORBA.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${from}")
     private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+
+
 }
